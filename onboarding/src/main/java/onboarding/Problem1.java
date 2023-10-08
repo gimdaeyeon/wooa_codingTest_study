e1ef458b161bbf35c6959690af6fdb8478397be7
gimdaeyeon <rlaeodus1306@naver.com>
2023-10-08 22:58:22 +0900

    init : 우테코 코딩테스트 연습용 파일, 리포지토리 생성

---
+++ onboarding/src/main/java/onboarding/Problem1.java
@@ -0,0 +1,11 @@
+package onboarding;
+
+import java.util.List;
+
+class Problem1 {
+    public static int solution(List<Integer> pobi, List<Integer> crong) {
+        int answer = Integer.MAX_VALUE;
+        System.out.println("test");
+        return answer;
+    }
+}
