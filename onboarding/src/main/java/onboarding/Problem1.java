f916a6715845911a44cafec1b19991b60c2734ab
gimdaeyeon <rlaeodus1306@naver.com>
2023-10-09 21:09:23 +0900

    no message

--- onboarding/src/main/java/onboarding/Problem1.java
+++ onboarding/src/main/java/onboarding/Problem1.java
@@ -1,11 +1,12 @@
 package onboarding;
 
+import com.sun.net.httpserver.HttpServer;
+
 import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
-        System.out.println("test");
         return answer;
     }
 }
