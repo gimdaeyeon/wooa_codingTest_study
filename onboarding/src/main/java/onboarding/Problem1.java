7e30af4c759d6ad8d14e553571cc7da46d9ffe25
gimdaeyeon <rlaeodus1306@naver.com>
2023-10-11 17:44:45 +0900

    WIP:온보딩 문제1 푸는 중

--- onboarding/src/main/java/onboarding/Problem1.java
+++ onboarding/src/main/java/onboarding/Problem1.java
@@ -4,7 +4,21 @@ import com.sun.net.httpserver.HttpServer;
 
 import java.util.List;
 
+/*
+* 1. 책을 임의로 펼친다.
+* 2. 왼쪽 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구한다.
+* 3. 오른쪽 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구한다.
+* 4. 2~3 과정에서 가장 큰 수를 본인의 점수로 한다.
+* 5. 점수를 비교해 가장 높은 사람이 게임의 승자가 된다.
+* 6. 시작 면이나 마지막 면이 나오도록 책을 펼치지 않는다.
+*/
+
 class Problem1 {
+    public static final int POBI_WIN_CASE =1;
+    public static final int CRONG_WIN_CASE =2;
+    public static final int TIE_CASE = 0;
+    public static final int EXCEPTION_CASE = 0;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
 
