17a6aea25b3e47a5f47ff942422d6b8a064f2237
gimdaeyeon <rlaeodus1306@naver.com>
2023-10-16 21:37:34 +0900

    WIP: 5번문제 진행중

--- onboarding/src/main/java/onboarding/Problem5.java
+++ onboarding/src/main/java/onboarding/Problem5.java
@@ -6,24 +6,57 @@ import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        Cashs cashs = Cashs.of(money);
+        return cashs.withDrawCash();
     }
 }
 
-enum Cashs{
-    CASH50000(0,50000), CASH10000(1,10000),
-    CASH5000(2,5000), CASH1000(3,1000),
-    CASH500(4,500), CASH100(5,100),
-    CASH50(6,50), CASH10(7,10), CASH1(8,1);
+class Cashs{
+    private List<Integer> list;
+
+    private Cashs(List<Integer> list, int money) {
+        this.list = list;
+        toBanknote(money);
+    }
+    public static Cashs of(int money){
+        Cashs cashs = new Cashs(new ArrayList<Integer>(BANKNOTES.values().length), money);
+        return cashs;
+    }
+
+    public List<Integer> withDrawCash(){
+        return list;
+    }
+
+    /*
+    * 이 메소드 안에서 각 현금별로 for 문을 돌린다.
+    * 각 현금의 값으로 while문을 돌려 해당 지폐가 몇개 필요한지 구해서 list에 추가하고,
+    * 해당 지폐로 변환하고 남은 돈을 반환한다
+    * */
+    public void toBanknote(int money){
+        for(BANKNOTES banknote: BANKNOTES.values()){
+
+        }
+    }
+
+
+
+
+}
+
+
+
+enum BANKNOTES{
+    BANKNOTE50000(0,50000), BANKNOTE10000(1,10000),
+    BANKNOTE5000(2,5000), BANKNOTE1000(3,1000),
+    BANKNOTE500(4,500), BANKNOTE100(5,100),
+    BANKNOTE50(6,50), BANKNOTE10(7,10), BANKNOTE1(8,1);
     private int index;
     private int value;
 
-    Cashs(int index, int value) {
+    BANKNOTES(int index, int value) {
         this.index = index;
         this.value = value;
     }
-
     public int getValue() {
         return value;
     }
@@ -31,5 +64,6 @@ enum Cashs{
     public int getIndex() {
         return index;
     }
+
 }
 
