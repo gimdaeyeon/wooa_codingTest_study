1945b2535254d65e0484e7fac437e4e7c4b88384
gimdaeyeon <rlaeodus1306@naver.com>
2023-10-16 19:15:36 +0900

    feat:  문제4 Alphabet클래스 생성, Map에 소, 대문자 세팅 메소드 추가

--- onboarding/src/main/java/onboarding/Problem4.java
+++ onboarding/src/main/java/onboarding/Problem4.java
@@ -1,8 +1,38 @@
 package onboarding;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
         return answer;
     }
+
+    static class Alphabet{
+        private Map<Character,Character> map;
+        private Alphabet(Map<Character,Character> map) {
+            this.map =map;
+            setAlphabets('a');
+            setAlphabets('A');
+        }
+
+        public static Alphabet of(){
+            Alphabet alphabet = new Alphabet(new HashMap<Character, Character>());
+            return alphabet;
+        }
+
+        private void setAlphabets(char start){
+
+            for (int i = start; i <=start+25 ; i++) {
+                map.put((char)i, (char)(start*2+25-i));
+            }
+        }
+
+
+    }
+
+
+
 }
