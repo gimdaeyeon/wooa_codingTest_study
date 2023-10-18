d69ea7cb8f08805fd6c189c968685c8878078050
gimdaeyeon <rlaeodus1306@naver.com>
2023-10-18 15:02:39 +0900

    feat: 문제7 기능구현 -  user의 친구 리스트 만들기

--- onboarding/src/main/java/onboarding/Problem6.java
+++ onboarding/src/main/java/onboarding/Problem6.java
@@ -13,31 +13,27 @@ public class Problem6 {
         return findDuplicateNicknames(forms).stream().distinct().sorted().collect(Collectors.toList());
     }
 
-    public static List<String> findDuplicateNicknames(List<List<String>> forms){
+    public static List<String> findDuplicateNicknames(List<List<String>> forms) {
         List<String> emailList = new ArrayList<>();
 
-        for (int i = 0; i < forms.size() ; i++) {
-            for (int j = 0; j < forms.get(i).get(NICKNAME_INDEX).length()-2; j++) {
-                String str = forms.get(i).get(NICKNAME_INDEX).substring(j,j+2);
+        for (int i = 0; i < forms.size(); i++) {
+            for (int j = 0; j < forms.get(i).get(NICKNAME_INDEX).length() - 2; j++) {
+                String str = forms.get(i).get(NICKNAME_INDEX).substring(j, j + 2);
                 for (int k = 0; k < forms.size(); k++) {
-                    if(k==i){
+                    if (k == i) {
                         continue;
                     }
-                    if(forms.get(k).get(NICKNAME_INDEX).contains(str)){
+                    if (forms.get(k).get(NICKNAME_INDEX).contains(str)) {
                         emailList.add(forms.get(k).get(EMAIL_INDEX));
                     }
 
                 }
 
             }
-
         }
-
 
         return emailList;
     }
-
-
 
 
 }
--- onboarding/src/main/java/onboarding/Problem7.java
+++ onboarding/src/main/java/onboarding/Problem7.java
@@ -1,11 +1,33 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Problem7 {
+    public static Map<String,Integer> scoreMap = new HashMap<>();
+    public static List<String> friendList = new ArrayList<>();
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+        makeUsersFriendList(user,friends);
+
+        return null;
     }
+
+    public static void makeUsersFriendList(String user, List<List<String>> friends ){
+
+        for(List<String> list: friends){
+            isUsersFriend(list,user);
+        }
+
+    }
+
+    public static void isUsersFriend(List<String> relation, String user){
+        if(relation.contains(user)){
+            friendList.add(relation.get(1-relation.indexOf(user)));
+        }
+    }
+
+
+
 }
